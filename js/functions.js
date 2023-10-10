20a408120c821b0240f75131f0c9a7f95fdb373f
Andrey Uhanov <aluhandrei@gmail.com>
2023-10-10 19:51:06 +0300

    Добавил две функции

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+const isAllowedLength = function (string, length) {
+  return string.length <= length;
+};
+isAllowedLength('проверяемая строка', 10);
+
+const isPalindrom = function (string) {
+  const newString = string.replaceAll(' ', '').toUpperCase();
+  let anotherString = '';
+  for (let i = newString.length - 1; i >= newString[0]; i--) {
+    anotherString += newString[i];
+  }
+  return string === anotherString;
+};
+isPalindrom('проверяемая строка');
